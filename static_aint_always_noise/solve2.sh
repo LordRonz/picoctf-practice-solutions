55fdbf1487fbae47255b4b5fac3687e5e3710fd0
LordRonz <aarontanhar2000@gmail.com>
2022-02-26 17:48:52 +0700

    fix: wrong shebang :rocket:

---
+++ static_aint_always_noise/solve2.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+strings static | grep "pico"
--- transformation/solve.py
+++ transformation/solve.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python
 
 # ''.join([chr((ord(flag[i]) << 8) + ord(flag[i + 1])) for i in range(0, len(flag), 2)])
 
